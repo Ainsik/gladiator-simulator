d79d952b259b2e8f3a3104e9ea10d66f1e4d6084
Ainsik <pio.andrzejczuk@gmail.com>
2023-03-18 12:15:18 +0100

    implement GetNumberBetween method

--- View/ConsoleView.cs
+++ View/ConsoleView.cs
@@ -2,7 +2,39 @@
 
 namespace Gladiator.View
 {
-    public class ConsoleView
+    public class ConsoleView : IView
     {
+        public string Display()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetNumberBetween(int min, int max)
+        {
+            Console.WriteLine($"Choose number between {min} - {max}");
+            var boolValueFromParse = ParseUserInputToNumber(min, max).Item1;
+
+            while (boolValueFromParse == false)
+            {
+                Console.WriteLine($"Wrong value!");
+                Console.WriteLine($"Choose number again between {min} - {max}");
+                boolValueFromParse = ParseUserInputToNumber(min, max).Item1;
+                if (boolValueFromParse)
+                {
+                    Console.WriteLine("ok");
+                    return true;
+                }
+            }
+
+            Console.WriteLine("OK");
+            return true;
+        }
+
+        private static (bool, int) ParseUserInputToNumber(int min, int max)
+        {
+            var success = int.TryParse(Console.ReadLine(), out var number);
+            var condition = success && number >= min && number <= max;
+            return (condition, number);
+        }
     }
 }
